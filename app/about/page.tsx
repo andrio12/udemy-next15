8cf62d10c1e903a374b0a44e705ec06a6ded299e
andrio12 <andri.olafsson@netapp.com>
2025-04-16 15:13:21 +0000

    about page, favicon

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+        <div className="flex flex-col gap-8 items-center justify-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <h1 className="text-2xl font-semibold">About</h1>
+            <p className="text-lg">
+                This is the about page of our Next.js application.
+            </p>
+        </div>
+    );
+}
